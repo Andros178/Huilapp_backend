3686f4a1e2da6d0b43432c4888c79ab3baadedb1
Setup <setup@yopmail.com>
2025-08-29 14:06:56 -0500

    setup de inicio backend con express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Example defining a route in Express
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, Express.js root endpoint Server!</h1>');
+});
+
+const usersRoute = require('./routes/users');
+const productsRoute = require('./routes/products');
+
+// Example specifying the port and starting the server
+
+const port = process.env.PORT || 3000; // You can use environment variables for port configuration
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
